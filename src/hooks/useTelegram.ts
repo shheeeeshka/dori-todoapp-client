ca8746df8d81fdeab43265f062959bb9009f1323
shheeeeshka <102031292+shheeeeshka@users.noreply.github.com>
2025-05-16 11:29:53 +0300

    Update useTelegram.ts

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -1,25 +1,48 @@
-const tg = window?.Telegram?.WebApp;
+interface TelegramWebApp {
+  WebApp?: {
+    close: () => void;
+    MainButton: {
+      isVisible: boolean;
+      show: () => void;
+      hide: () => void;
+    };
+    initDataUnsafe?: {
+      user?: any;
+      query_id?: string;
+    };
+    version?: string;
+    colorScheme?: string;
+  };
+}
+
+declare global {
+  interface Window {
+    Telegram?: TelegramWebApp;
+  }
+}
 
 export function useTelegram() {
-    const onClose = () => {
-        tg.close()
-    }
+  const tg = window.Telegram?.WebApp;
 
-    const onToggleButton = () => {
-        if (tg.MainButton.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }
+  const onClose = () => {
+    tg?.close();
+  };
 
-    return {
-        onClose,
-        onToggleButton,
-        tg,
-        tgUser: tg?.initDataUnsafe?.user,
-        queryId: tg?.initDataUnsafe?.query_id,
-        tgVersion: tg?.version,
-        tgColorScheme: tg?.colorScheme,
+  const onToggleButton = () => {
+    if (tg?.MainButton.isVisible) {
+      tg.MainButton.hide();
+    } else {
+      tg?.MainButton.show();
     }
+  };
+
+  return {
+    onClose,
+    onToggleButton,
+    tg,
+    tgUser: tg?.initDataUnsafe?.user,
+    queryId: tg?.initDataUnsafe?.query_id,
+    tgVersion: tg?.version,
+    tgColorScheme: tg?.colorScheme,
+  };
 }
